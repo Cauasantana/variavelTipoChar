3f7324179f7b83fee6b5819ce713cb0dd07a26e4
Caua Santana <santanacaua155@gmail.com>
2022-09-13 15:37:28 -0300

    variavel tipo char

---
+++ variavelChar/src/variavelChar/variavelTipoChar.java
@@ -0,0 +1,11 @@
+package variavelChar;
+
+public class variavelTipoChar {
+	
+	//metodo main é um metodo auto executavel no java
+			public static void main(String[] args) {
+				
+				//CHAR representa uma letra
+				
+			}
+}
